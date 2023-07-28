232ef07dc4d2af6cff0ee1db99c212021c10b955
Iam-abhi121 <abhijeetpatil572000@gmail.com>
2023-07-29 02:02:40 +0530

    updating config file

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 PORT_NUMBER = 8080
 
-MODEL_FILE_PATH = r'static\model\decision_tree_pruning.pkl'
-JSON_FILE_PATH = r'static\json_files\proj_data.json'
+MODEL_FILE_PATH = os.path.join(r'static','model','decision_tree_pruning.pkl')
+JSON_FILE_PATH = os.path.join(r'static','json_files','proj_data.json')
